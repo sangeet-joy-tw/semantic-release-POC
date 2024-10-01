f49377b7633772a8866cc0bb7b89daef0cab878e
sangeet-joy_xero <sangeet.joy@xero.com>
2024-10-01 18:17:29 +0530

    fix: correctex the release config

--- .github/semantic-release/release.config.js
+++ .github/semantic-release/release.config.js
@@ -1,19 +1,16 @@
 const config = {
     branches: ['main'],
     tagFormat: "${version}",
-    Plugins: [
-        '@semantic-release/commit-analyzer',
-        ['@semantic-release/release-notes-generator', {
-            "preset": "angular",
-            "writerOpts": {
-                "headerPartial": "#{{version}}\n",
-                "footerPartial": "\n**Full Changelog**:https://github.com/XeroAPI/Xero-OpenAPI/compare/{{previousTag}}...{{version}}"
+    plugins: [
+        ['@semantic-release/release-notes-generator',
+            {
+                preset: 'angular',
+                writerOpts: {
+                  headerPartial: '# {{version}}\n',
+                  footerPartial: '\n**Full Changelog**:https://github.com/XeroAPI/Xero-OpenAPI/compare/{{previousTag}}...{{version}}'
+                }
             }
-        }],
-        '@semantic-release/changelog',
-        '@semantic-release/github',
-        '@semantic-release/git',
+         ]
     ]
 }
-
-module.exports = config
+module.exports = config;
