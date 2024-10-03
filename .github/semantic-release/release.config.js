27f5c5273a45d62dbb475c23faf104944741b70a
sangeet-joy_xero <sangeet.joy@xero.com>
2024-10-03 15:11:08 +0530

    feat: added the breaking change function
    BREAKING CHANGE: new breaking change description

--- .github/semantic-release/release.config.js
+++ .github/semantic-release/release.config.js
@@ -6,7 +6,6 @@ const config = {
             {
                 preset: 'angular',
                 writerOpts: {
-                  headerPartial: '# {{version}}\n',
                   footerPartial: '\n**Full Changelog**:https://github.com/XeroAPI/Xero-OpenAPI/compare/{{previousTag}}...{{version}}'
                 }
             }
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 console.log("hello world typo fix");
 console.log("Hello from another world");
 console.log("Hello");
+console.log("Breaking change function")
